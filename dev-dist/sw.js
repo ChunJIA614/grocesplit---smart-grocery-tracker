d6138ce37be5328868b483561487de90d3c83e37
Chun Jia <139411940+ChunJIA614@users.noreply.github.com>
2026-02-01 19:37:14 +0800

    minor change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dth2tjpp3t8"
+    "revision": "0.3cg6m7lj0uo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
